eaa550cf97c181d57ecb1ca00772770017d57e45
davilepore <davilepore218@gmail.com>
2026-02-20 17:40:17 -0300

    feat(style): add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Libre_Baskerville } from "next/font/google";
+
+const libre = Libre_Baskerville({
+  subsets: ["latin"],
+  weight: ["400", "700"], // normal e bold
+});
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,12 +30,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-br">
+      <body className={libre.className}>{children}</body>
     </html>
   );
 }
